2eec00877568697fff448e9255f875afd7c01428
Ritz <ritzdacanay24@yahoo.com>
2024-07-16 07:54:16 -0700

    Updates

--- src/app/pages/field-service/maintenance/licensed-techs/license-entity-constant.ts
+++
@@ -1,4 +0,0 @@
-export class NAVIGATION_ROUTE {
-  public static readonly CREATE = '/dashboard/field-service/maintenance/licensed-techs/create';
-  public static readonly EDIT = '/dashboard/field-service/maintenance/licensed-techs/edit';
-}
