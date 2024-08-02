5ccb729c50f3a5190662fb91dcde0b3564842efd
Ritz Dacanay <rdacanay@Eye-Fi.local>
2024-08-02 13:34:49 -0700

    Updates

--- src/app/pages/maintenance/user/user-constant.ts
+++ src/app/pages/maintenance/user/user-constant.ts
@@ -20,7 +20,7 @@ export const departments = [
 export const accessRight = [
   { name: "Regular", value: 0, bgColor: "#3AB6E3" },
   { name: "Lead", value: 1, bgColor: "#85144b" },
-  { name: "Supervisor", value: 2, bgColor: "#orange" },
+  { name: "Supervisor", value: 2, bgColor: "orange" },
   { name: "Manager", value: 3, bgColor: "#662d91" },
   { name: "Director", value: 4, bgColor: "#17B169" },
   { name: "CEO", value: 5, bgColor: "black" },
